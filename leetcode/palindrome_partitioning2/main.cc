e9b83d9045ab6eccbc649f09d47fa5b612303bc8
WangFengwei <foomango@gmail.com>
2013-09-09 00:36:22 +0800

    Daily commit

---
+++ leetcode/palindrome_partitioning2/main.cc
@@ -0,0 +1,92 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cc
+ *
+ *    Description:  http://foomango.blog.163.com/blog/static/218098074201386103118218/
+ *
+ *        Version:  1.0
+ *        Created:  09/05/2013 09:51:00 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  WangFengwei (mn), foomango@gmail.com
+ *        Company:  HUAZHONG UNIVERSITY OF SCIENCE AND TECHNOLOGY
+ *
+ * =====================================================================================
+ */
+
+#include <string>
+#include <algorithm>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int minCut(string s) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        if (s.size() == 0) {
+            return 0;
+        }
+        m_cutMap.clear();
+        m_palindromeMap.clear();
+        minCutRecursive(s);
+        
+    }
+private:
+    int minCutRecursive(const string& s) {
+        map<string, int>::iterator it = m_cutMap.find(s);
+        if (it != m_cutMap.end()) {
+            return (*it).second;
+        }
+
+        if (isPalindrome(s)) {
+            m_cutMap.insert(pair<string, int>(s, 0));
+            return 0;
+        }
+
+        int ret = s.size() - 1;
+        for (int i = 1; i < s.size(); ++i) {
+            ret = min(ret, minCutRecursive(s.substr(0, i)) + minCutRecursive(s.substr(i, s.size())) + 1);
+        }
+
+        m_cutMap.insert(pair<string, int>(s, ret));
+        return ret;
+    }
+
+    bool isPalindrome(const string& s) {
+        map<string, bool>::iterator it = m_palindromeMap.find(s);
+        if (it != m_palindromeMap.end()) {
+            return (*it).second;
+        }
+
+        int i = 0;
+        int j = s.size() - 1;
+        int ret = true;
+        while (i < j) {
+            if (s.at(i++) != s.at(j--)) {
+                ret = false;
+            }
+        }
+
+        m_palindromeMap.insert(pair<string, bool>(s, ret));
+
+        return ret;
+    }
+
+    map<string, int> m_cutMap;
+    map<string, bool> m_palindromeMap;
+
+};
+
+int main(int argc, char *argv[]) 
+{
+    Solution s;
+    cout << "a:" << s.minCut("a") << endl;
+    cout << "ab:" << s.minCut("ab") << endl;
+    cout << "abbacd:" << s.minCut("abbacd") << endl;;
+    return 0;
+}
---
+++ leetcode/palindrome_partitioning2/main2.cc
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main2.cc
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  09/06/2013 10:25:18 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  WangFengwei (mn), foomango@gmail.com
+ *        Company:  HUAZHONG UNIVERSITY OF SCIENCE AND TECHNOLOGY
+ *
+ * =====================================================================================
+ */
+
+
